f797aea5efab80b78c86097a6cfa47fbf4a7cc84
start.vaadin.com <no-reply@vaadin.com>
2024-01-02 12:48:12 +0000

    Generated project

---
+++ src/main/java/com/udla/productos/views/listarproductos/ListarProductosView.java
@@ -0,0 +1,20 @@
+package com.udla.productos.views.listarproductos;
+
+import com.udla.productos.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Listar Productos")
+@Route(value = "listar-productos", layout = MainLayout.class)
+@Uses(Icon.class)
+public class ListarProductosView extends Composite<VerticalLayout> {
+
+    public ListarProductosView() {
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+    }
+}
